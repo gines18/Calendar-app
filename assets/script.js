e9e9b017e811eef37697fba9de4c3f7c2cc30540
Mario <ruczkowski.m@gmail.com>
2023-01-27 11:06:16 +0000

    Add getItem method for localStorage using jguery for each hour

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,9 @@
 //Display current day.
-
 let currentTime = document.getElementById("#currentTime");
 let currentDay = moment().format("dddd, MMMM Do");
 $("#currentDay").text(currentDay);
 
 // .on click function referring to the buttons.   
-
 $(".saveBtn").on("click", function(){
   let textInput = $(this).siblings(".description").val();
   let timeStamp = $(this).parent().attr("id");
@@ -31,4 +29,13 @@ let currentHour = moment().hours();
 };
 actualTime()
 
+$("#9 .description").val(localStorage.getItem("9"));
+$("#10 .description").val(localStorage.getItem("10"));
+$("#11 .description").val(localStorage.getItem("11"));
+$("#12 .description").val(localStorage.getItem("12"));
+$("#13 .description").val(localStorage.getItem("13"));
+$("#14 .description").val(localStorage.getItem("14"));
+$("#15 .description").val(localStorage.getItem("15"));
+$("#16 .description").val(localStorage.getItem("16"));
+$("#17 .description").val(localStorage.getItem("17"));
 
