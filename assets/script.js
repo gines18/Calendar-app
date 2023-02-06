4bb9f16a45fa02e15a0a0eb7b454b5b9aeab9bdc
Mario <ruczkowski.m@gmail.com>
2023-02-06 19:02:40 +0000

    add margin bottom for last textarea, change doubled id from 12pm to 13pm in index.html file

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,16 @@
-//Display current day.
+//Display current date
 let currentTime = document.getElementById("#currentTime");
 let currentDay = moment().format("dddd, MMMM Do");
 $("#currentDay").text(currentDay);
 
-// .on click function referring to the buttons.   
+// .on click function referring to the buttons 
 $(".saveBtn").on("click", function(){
   let textInput = $(this).siblings(".description").val();
   let timeStamp = $(this).parent().attr("id");
   localStorage.setItem(timeStamp, textInput);
 });
 
-// Function displaying textarea background-color depends on current time.
+// Function displaying textarea background-color depends on current time
 let actualTime = function() {
 let currentHour = moment().hours();
   $(".row").each(function(){
@@ -29,6 +29,8 @@ let currentHour = moment().hours();
 };
 actualTime()
 
+//Retrieve user tasks from local storage
+let savedItems = 
 $("#9 .description").val(localStorage.getItem("9"));
 $("#10 .description").val(localStorage.getItem("10"));
 $("#11 .description").val(localStorage.getItem("11"));
@@ -38,4 +40,5 @@ $("#14 .description").val(localStorage.getItem("14"));
 $("#15 .description").val(localStorage.getItem("15"));
 $("#16 .description").val(localStorage.getItem("16"));
 $("#17 .description").val(localStorage.getItem("17"));
+
 
